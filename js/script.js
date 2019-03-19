331dddd223ce786fa57f9a5ba1e8c40de468eee2
Daniel Balcer <kemaron@gmail.com>
2019-03-19 20:25:01 +0100

    Add style

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ button.addEventListener('click', function(){
 
 var paragraph = document.getElementById('joke');
 
+// pobieram zart ze strony wskazanej w 'url', odpowiedź parsuje przy pomocy JSON
 function getJoke() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
@@ -17,4 +18,5 @@ function getJoke() {
     xhr.send();
   }
 
+//wywołanie po pierwszym wczytaniu
 getJoke();
